45e11f06c35f0b8c84aa6859d8f4171a9b05e550
Rasel-45 <mrraselch45@gmail.com>
2026-02-26 22:55:47 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+function login(){
+
+let user=document.getElementById("user").value;
+
+let pass=document.getElementById("pass").value;
+
+
+
+if(user=="admin" && pass=="1234"){
+
+window.location="idcard.html";
+
+}
+
+else{
+
+document.getElementById("error").innerHTML="Wrong Password";
+
+}
+
+}
+
+
+
+
+function generate(){
+
+let name=document.getElementById("name").value;
+
+let roll=document.getElementById("roll").value;
+
+let address=document.getElementById("address").value;
+
+let class=document.getElementById("class").value;
+
+let photo=document.getElementById("photo").files[0];
+
+
+
+document.getElementById("sname").innerHTML="Name: "+name;
+
+document.getElementById("sroll").innerHTML="Roll: "+roll;
+
+document.getElementById("saddress").innerHTML="Address: "+address;
+
+document.getElementById("sclass").innerHTML="Class: "+sclass;
+
+
+
+let reader=new FileReader();
+
+reader.onload=function(e){
+
+document.getElementById("img").src=e.target.result;
+
+}
+
+reader.readAsDataURL(photo);
+
+}
+
+
+
+function download(){
+
+html2canvas(document.querySelector("#card")).then(canvas => {
+
+let link=document.createElement('a');
+
+link.download='IDCard.png';
+
+link.href=canvas.toDataURL();
+
+link.click();
+
+});
+
+}
